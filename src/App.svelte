b5c7a12fa2f127abe699c907aa2352dbc1d7f792
Damon <damon@edstrom.io>
2025-08-07 17:29:02 -0500

    Init

---
+++ src/App.svelte
@@ -0,0 +1,137 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import Testimonial from "./lib/Testimonial.svelte";
+</script>
+
+
+<!--
+Grid Layout Pseudo
+a a b c
+d e e c
+
+Sizing
+
+Avatar Image - 30p x 30p
+Name + Img - 30 x 140-150p
+
+-->
+<main>
+  <div class="card-grid">
+    <section class="grid-c-a">
+      <Testimonial
+          custName="Daniel Clifford"
+          custTitle="Verified Graduate"
+          highlight="I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth."
+          testimonial="  “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup.”"
+          accentImg="Render Me Please!"
+          imgBrColor="purple"
+      />
+    </section>
+    <section class="grid-c-b">
+      <Testimonial
+          custName="Jonathan Walters"
+          custAvatar="image-jonathan.jpg"
+          cardColor="grey-dk"
+          highlight="The team was very supportive and kept me motivated"
+          testimonial="“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer
+  for a big company. This was one of the best investments I’ve made in myself. ”"
+          imgBrColor="grey"
+      />
+    </section>
+    
+    <section class="grid-c-d">
+      <Testimonial
+          custName="Jeanette Harmon"
+          custAvatar="image-jeanette.jpg"
+          cardColor="white-li"
+          cardText="grey-dk"
+          highlight="An overall wonderful and rewarding experience"
+          testimonial="“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living
+  while doing something I love. ”"
+      />
+    </section>
+    <section class="grid-c-e">
+      <Testimonial
+          custName="Patrick Abrams"
+          custAvatar="image-patrick.jpg"
+          cardColor="black-theme"
+          cardText="grey-xli"
+          highlight="Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and
+  learning from their experiences was easy."
+          testimonial="“ The staff seem genuinely concerned about my progress which I find really refreshing. The program
+  gave me the confidence necessary to be able to go out in the world and present myself as a capable
+  junior developer. The standard is above the rest. You will get the personal attention you need from
+  an incredible community of smart and amazing people. ”"
+          imgBrColor="purple"
+      />
+    </section>
+    <section class="grid-c-c">
+      <Testimonial
+          custName="Kira Whittle"
+          custAvatar="image-kira.jpg"
+          cardColor="white-li"
+          cardText="grey-dk"
+          highlight="Such a life-changing experience. Highly recommended!"
+          testimonial="“ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from
+  professionals who can help me learn programming step by step. I was encouraged to enroll by a former
+  student of theirs who can only say wonderful things about the program. The entire curriculum and staff
+  did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team
+  project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial
+  could ever have. In fact, I’ve often referred to it during interviews as an example of my developent
+  experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers.
+  100% recommend! ”"
+      />
+    </section>
+  
+  </div>
+</main>
+
+<style>
+  
+  .card-grid {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 2rem;
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 100%;
+    box-sizing: border-box;
+    grid-template-areas: none;
+    grid-template-rows: none;
+  }
+  
+  @media (min-width: 900px) {
+    .card-grid {
+      grid-template-columns: repeat(4, 1fr);
+      grid-template-areas:
+      "a a b c"
+      "d e e c";
+      grid-template-rows: 1fr 1fr;
+    }
+    
+    .grid-c-a {
+      grid-area: a;
+    }
+    
+    .grid-c-b {
+      grid-area: b;
+    }
+    
+    .grid-c-c {
+      grid-area: c;
+    }
+    
+    .grid-c-d {
+      grid-area: d;
+    }
+    
+    .grid-c-e {
+      grid-area: e;
+    }
+  }
+  
+  
+
+</style>
---
+++ src/lib/Testimonial.svelte
@@ -0,0 +1,84 @@
+<script>
+  let {
+    custName = 'Customer Name',
+    custTitle = 'Verified Graduate',
+    custAvatar = 'image-daniel.jpg',
+    imgBrColor = 'white',
+    highlight = 'Customer testimonial highlight',
+    testimonial = 'Testimonial body content',
+    cardColor = 'purple-dk',
+    cardText = 'white-li',
+    accentImg = ""
+  } = $props()
+  
+  const bgColorMap = {
+    'purple-li': 'bg-purple-li',
+    'purple-md': 'bg-purple-md',
+    'purple-dk': 'bg-purple-dk',
+    'grey-dk': 'bg-grey-dk',
+    'white-li': 'bg-white',
+    'black-theme': 'bg-black-theme'
+  }
+  const txtColorMap = {
+    'grey-xli': 'text-grey-xli',
+    'grey-md': 'text-grey-md',
+    'grey-dk': 'text-grey-dk',
+    'white-li': 'text-white',
+  }
+  
+  const bdrColorMap = {
+    'purple': 'border-purple-md',
+    'white':  'border-grey-li',
+    'grey':   'border-grey-dk',
+  }
+
+</script>
+
+<style>
+
+
+</style>
+
+<!--
+Padding (px)
+Left:   30
+Right:  35
+Top:    30
+Bottom: 35
+-->
+
+<div class="
+          min-h-[275px] min-w-0 w-full max-w-full sm:min-w-[250px] sm:max-w-[545px]
+          {bgColorMap[cardColor]} {txtColorMap[cardText]}
+          rounded-xl shadow-lg shadow-gray-400 p-7.5
+          text-left relative z-10 h-full"
+>
+  <!--  TODO: Find way to align image with the two lines of text properly -->
+  
+  <section class="flex flex-row align-center items-center text-left pb-4">
+    <!--    TODO: Figure out image border w/ rounded edges-->
+    <img
+        src="{import.meta.env.BASE_URL + custAvatar}"
+        alt="CustImg"
+        class="rounded-full object-cover h-[30px] w-[30px] border-2 {bdrColorMap[imgBrColor]} mr-3 "
+    >
+    <div>
+      <p class="text-sm font-medium">{custName}</p>
+      <p class="text-xs font-extralight">{custTitle}</p>
+    </div>
+  </section>
+  <section class="relative text-xl text-left z-10 pb-4 font-bold">
+    {highlight}
+  </section>
+  <article class="text-[13px] font-normal ">
+    {testimonial}
+  </article>
+  {#if accentImg !== ""}
+    <img
+        src="{import.meta.env.BASE_URL + 'bg-pattern-quotation.svg'}"
+        alt="QuoteAccent"
+        class="absolute top-0 right-6 md:right-8 z-0"
+    />
+  {/if}
+</div>
+
